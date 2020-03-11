179200a3065db71dd247eb4a13ebeeb35a5893a0
Federico García <fede@divinodi.com>
2020-03-11 16:30:08 -0300

    API básica (albums, artists, tracks)

---
+++ server/modules/API.js
@@ -0,0 +1,97 @@
+const Artist = require('./../models/artist');
+const Album = require('./../models/album');
+const Track = require('./../models/track');
+const mongoose = require('mongoose');
+
+const API = {
+  paging: 10,
+
+  artists: async function(req, res) {
+    const skip = (!isNaN(req.query.skip) ? parseInt(req.query.skip) : 0);
+    const limit = (!isNaN(req.query.limit) ? parseInt(req.query.limit) : API.paging);
+
+    const artists = await Artist.find(null, { __v: 0 }, { skip, limit });
+    const total = await Artist.countDocuments();
+
+    res.send(JSON.stringify({ artists, total }));
+  },
+
+  artist: async function(req, res) {
+    if(!mongoose.isValidObjectId(req.params.id)) {
+      return res.send(JSON.stringify({ error: "Invalid ID" }));
+    }
+
+    const artist = await Artist.findOne({ _id: req.params.id }, { __v: 0 });
+    res.send(JSON.stringify(artist));
+  },
+
+  artistAlbums: async function(req, res) {
+    if(!mongoose.isValidObjectId(req.params.id)) {
+      return res.send(JSON.stringify({ error: "Invalid ID" }));
+    }
+
+    const skip = (!isNaN(req.query.skip) ? parseInt(req.query.skip) : 0);
+    const limit = (!isNaN(req.query.limit) ? parseInt(req.query.limit) : API.paging);
+    
+    const albums = await Album.find({ artistId: req.params.id }, { __v: 0 }, { skip, limit });
+    const total = await Album.countDocuments({ artistId: req.params.id });
+
+    res.send(JSON.stringify({ albums, total }));
+  },
+
+  artistTracks: async function(req, res) {
+    if(!mongoose.isValidObjectId(req.params.id)) {
+      return res.send(JSON.stringify({ error: "Invalid ID" }));
+    }
+
+    const skip = (!isNaN(req.query.skip) ? parseInt(req.query.skip) : 0);
+    const limit = (!isNaN(req.query.limit) ? parseInt(req.query.limit) : API.paging);
+    
+    const tracks = await Track.find({ artistId: req.params.id }, { __v: 0 }, { skip, limit });
+    const total = await Track.countDocuments({ artistId: req.params.id });
+
+    res.send(JSON.stringify({ tracks, total }));
+  },
+
+  albums: async function(req, res) {
+    const skip = (!isNaN(req.query.skip) ? parseInt(req.query.skip) : 0);
+    const limit = (!isNaN(req.query.limit) ? parseInt(req.query.limit) : API.paging);
+
+    const albums = await Album.find(null, { __v: 0 }, { skip, limit });
+    const total = await Album.countDocuments();
+
+    res.send(JSON.stringify({ albums, total }));
+  },
+
+  album: async function(req, res) {
+    if(!mongoose.isValidObjectId(req.params.id)) {
+      return res.send(JSON.stringify({ error: "Invalid ID" }));
+    }
+
+    const album = await Album.findOne({ _id: req.params.id }, { __v: 0 });
+    res.send(JSON.stringify(album));
+  },
+
+  albumTracks: async function(req, res) {
+    if(!mongoose.isValidObjectId(req.params.id)) {
+      return res.send(JSON.stringify({ error: "Invalid ID" }));
+    }
+    const tracks = await Track.find({ albumId: req.params.id }, { __v: 0 });
+    const total = await Track.countDocuments({ albumId: req.params.id });
+
+    res.send(JSON.stringify({ tracks, total }));
+  },
+
+  tracks: async function(req, res) {
+    const skip = (!isNaN(req.query.skip) ? parseInt(req.query.skip) : 0);
+    const limit = (!isNaN(req.query.limit) ? parseInt(req.query.limit) : API.paging);
+
+    const tracks = await Track.find(null, { __v: 0 }, { skip, limit });
+    const total = await Track.countDocuments();
+
+    res.send(JSON.stringify({ tracks, total }));
+  },
+
+};
+
+module.exports = API;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const path = require('path');
 const streamer = require('./modules/streamer.js')
+const mongoose = require('mongoose');
+const API = require('./modules/API.js')
+
+mongoose.connect(process.env.MUSIFY_MONGODB_SERVER, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 const port = 8080;
 
 app.get('/files/*', streamer);
+
+app.get('/api/artists', API.artists);
+app.get('/api/artists/:id', API.artist);
+app.get('/api/artists/:id/albums', API.artistAlbums);
+app.get('/api/artists/:id/tracks', API.artistTracks);
+app.get('/api/albums', API.albums);
+app.get('/api/albums/:id', API.album);
+app.get('/api/albums/:id/tracks', API.albumTracks);
+app.get('/api/tracks', API.tracks);
 
 app.listen(port, () => {
   console.log('Server started!');
